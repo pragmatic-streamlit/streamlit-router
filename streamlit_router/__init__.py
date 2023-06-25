bc9deb6cd8caad021cc3df214ff89266e008aa8c
luowf <louwf@dp.tech>
2023-06-25 16:14:56 +0800

    add endpoint to session

--- streamlit_router/__init__.py
+++ streamlit_router/__init__.py
@@ -6,16 +6,26 @@ from functools import wraps
 from werkzeug.routing import Map, Rule
 import streamlit as st
 
-class AttributeDict(dict): 
+
+class AttributeDict(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
 
+
 class StreamlitRouter:
-    """ for syntax @
+    """for syntax @
     https://werkzeug.palletsprojects.com/en/2.0.x/routing/
     """
 
-    def __init__(self, default_path='/', inject_name='router', *, session_prefix='streamlit-router-prefix:', state_name='streamlit-router-state'):
+    def __init__(
+        self,
+        default_path="/",
+        inject_name="router",
+        *,
+        session_prefix="streamlit-router-prefix:",
+        state_name="streamlit-router-state",
+        endpoint_session_name="streamlit-router-endpoint",
+    ):
         self._map = Map()
         self.view_methods = {}
         self.views = {}
@@ -23,20 +33,29 @@ class StreamlitRouter:
         self.inject_name = inject_name
         self.state_name = state_name
         self.session_prefix = session_prefix
+        self.endpoint_session_name = endpoint_session_name
         self.urls = self._map.bind("", "/")
         if not st.session_state.get(self.state_name, None):
             setattr(st.session_state, self.state_name, AttributeDict())
 
-    def register(self, func: typing.Callable, path: str, methods: typing.List[str] = None, endpoint: str = None):
+    def register(
+        self,
+        func: typing.Callable,
+        path: str,
+        methods: typing.List[str] = None,
+        endpoint: str = None,
+    ):
         return self.map(path, methods, endpoint)(func)
 
-    def map(self, path: str, methods: typing.List[str] = None, endpoint: str = None):  # pylint: disable=unused-argument
+    def map(
+        self, path: str, methods: typing.List[str] = None, endpoint: str = None
+    ):  # pylint: disable=unused-argument
         def _(func):
             nonlocal endpoint, methods
             if not endpoint:
                 endpoint = func.__name__
             if not methods:
-                methods = ['GET']
+                methods = ["GET"]
             self.views[endpoint] = func
             self._map.add(Rule(path, methods=methods, endpoint=endpoint))
             self.view_methods[endpoint] = methods
@@ -45,16 +64,22 @@ class StreamlitRouter:
             def wraped(*args, **kwargs):
                 if args:
                     raise AssertionError(
-                        'positon style args not allowed for route func')
+                        "positon style args not allowed for route func"
+                    )
                 argspec = inspect.getfullargspec(func).args
                 if self.inject_name in argspec:
                     kwargs[self.inject_name] = self
                 return func(*args, **kwargs)
+
             return wraped
+
         return _
 
     def handle(self, path: str, method: str = None):
-        endpoint, kwargs = self.urls.match(path, method) # pylint: disable=unpacking-non-sequence
+        endpoint, kwargs = self.urls.match(
+            path, method
+        )  # pylint: disable=unpacking-non-sequence
+        st.session_state[self.endpoint_session_name] = endpoint
         func = self.views[endpoint]
         argspec = inspect.getfullargspec(func).args
         if self.inject_name in argspec:
@@ -63,14 +88,14 @@ class StreamlitRouter:
 
     def redirect(self, path: str, method: str = None):
         self.reset_request_state()
-        st.session_state['request'] = (path, method)
-        st.session_state['request_id'] = uuid4().hex
+        st.session_state["request"] = (path, method)
+        st.session_state["request_id"] = uuid4().hex
         st.experimental_rerun()
-        
+
     def get_request_id(self):
-        return st.session_state.get('request_id', uuid4().hex)
-    
-    def get_request_state(self, name: str=None, default=None):
+        return st.session_state.get("request_id", uuid4().hex)
+
+    def get_request_state(self, name: str = None, default=None):
         if st.session_state.get(self.state_name, None) is None:
             st.session_state[self.state_name] = AttributeDict()
         state = st.session_state.get(self.state_name)
@@ -79,19 +104,19 @@ class StreamlitRouter:
         if state.get(name, None) is None:
             state[name] = default
         return state.get(name)
-    
+
     def delete_request_state(self, name: str):
         if st.session_state.get(self.state_name, None) is None:
             st.session_state[self.state_name] = AttributeDict()
         state = st.session_state.get(self.state_name)
         return state.pop(name, None)
-    
+
     def set_request_state(self, name: str, value: typing.Any):
         if st.session_state.get(self.state_name, None) is None:
             st.session_state[self.state_name] = AttributeDict()
         state = st.session_state.get(self.state_name)
         state[name] = value
-    
+
     def reset_request_state(self):
         if st.session_state.get(self.state_name, None) is not None:
             setattr(st.session_state, self.state_name, AttributeDict())
@@ -105,16 +130,16 @@ class StreamlitRouter:
         return self.urls.build(endpoint, values), method
 
     def serve(self):
-        request = st.session_state.get('request')
+        request = st.session_state.get("request")
         query_string = st.experimental_get_query_params()
         if request:
             self.handle(*request)
             path, method = request
-            query_string['request'] = [f'{method}:{path}']
+            query_string["request"] = [f"{method}:{path}"]
             st.experimental_set_query_params(**query_string)
-        elif 'request' in query_string:
-            method, path = query_string.get('request')[0].split(':')
-            st.session_state['request'] = (path, method)
+        elif "request" in query_string:
+            method, path = query_string.get("request")[0].split(":")
+            st.session_state["request"] = (path, method)
             st.experimental_rerun()
         else:
             self.handle(self.default_path)
